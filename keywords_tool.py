f3648c8d2400f50accb311545a3cb0dd03da2393
yecohe <cohen.ye@gmail.com>
2025-03-03 10:48:32 +0200

    Update keywords_tool.py

--- keywords_tool.py
+++ keywords_tool.py
@@ -13,10 +13,14 @@ def run(client):
         "Hebrew (he)": "he"
     }
 
+    region_options = {
+        "Israel (il)": "il",
+        "Palestinian (ps)": "ps"
+    }
+
     engine_options = {
         "Pip Library": "library",
         "API Service": "API",
-        "Scraping": "homemade"
     }
 
     # Inputs for Keywords Search
@@ -26,15 +30,22 @@ def run(client):
         help="Enter the keywords you want to search for. Use commas to separate multiple keywords.")
         
         # Language and Max Results in the same row
-        col1, col2 = st.columns(2)
+        col1, col2, col3 = st.columns(3)
         with col1:
             selected_language = st.selectbox(
                 "Language:",
                 options=list(language_options.keys()),
                 help="Choose the language for the google search."
             )
-            language = language_options[selected_language]  # Get the language code
+            language = language_options[selected_language]  
         with col2:
+            selected_region = st.selectbox(
+                "Region:",
+                options=list(region_options.keys()),
+                help="Choose the region for the google search."
+            )
+            region = region_options[selected_region]  
+        with col3:
             limit = st.selectbox(
                 "Max Results:",
                 options=[100, 50, 10],
@@ -73,9 +84,9 @@ def run(client):
             keywords_query = re.split(r"[,\n]", keywords_query) # Split by commas and linebreaks
             keywords_query = [kw.strip() for kw in keywords_query if kw.strip()]  # Remove extra spaces and ignore empty strings
 
-            st.write(f"**Keywords List:** {keywords_query} | **Language:** {language} | **Number of Results:** {limit} | **Include 'inurl':** {'Yes' if include_inurl else 'No'} | **Homapage only:** {'Yes' if homepage_only else 'No'}")
+            st.write(f"**Keywords List:** {keywords_query} | **Language:** {language} | **Region:** {region} | **Number of Results:** {limit} | **Include 'inurl':** {'Yes' if include_inurl else 'No'} | **Homapage only:** {'Yes' if homepage_only else 'No'}")
 
             # Call the process_keywords function with the selected limit
             sheet_id = st.secrets["google_id"]
-            process_keywords(client, sheet_id, keywords_query, lang=language, inurl=include_inurl, limit=limit, homepage=homepage_only, engine=engine)
+            process_keywords(client, sheet_id, keywords_query, lang=language, inurl=include_inurl, limit=limit, homepage=homepage_only, engine=engine, country=region)
             st.info("The URLs were added to the file.")
